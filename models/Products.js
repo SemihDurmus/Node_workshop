7461eecde87e34b57d1a4325593412199f8b9689
SemihDurmus <semihdurmus@yahoo.com>
2020-12-09 21:14:53 +0100

    Add products.js

---
+++ models/Products.js
@@ -0,0 +1,74 @@
+const { Sequelize, DataTypes, Models } = require("sequelize");
+
+const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_PORT, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected successfully");
+  })
+  .catch(() => {
+    console.log("Connection Failed");
+  });
+
+class Product extends Model {}
+
+const properties = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    field: "ID", //tabloda hangi isimle gozuksun?
+  },
+
+  productName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "ProductName",
+  },
+  unitPrice: {
+    type: DataTypes.DECIMAL,
+    allowNull: true,
+    defaultValue: 0,
+    field: "UnitPrice",
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+    field: "Stock",
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATEONLY,
+    defaultValue: Sequelize.NOW,
+    //defaultValue : Date.now(),
+    field: "CreatedDate",
+  },
+  updatedAt: {
+    type: DataTypes.DATEONLY,
+    field: "UpdatedDate",
+  },
+  deletedAt: {
+    type: DataTypes.DATEONLY,
+    field: "DeletedDate",
+  },
+};
+
+const options = {
+  sequelize,
+  modelName: "Product",
+  //tableName: "Products",
+};
+
+Product.init(properties, options);
+
+module.exports = Product;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
+
+/**
+ * ---MVC temel duzeyde url'ler----
+ *
+ * www.w3schools.com/home
+ * /about
+ * /contact
+ *
+ * /products
+ * /products/add
+ * /products/update
+ * /products/:id/update
+ * /products/update
+ * /products/:id/delete
+ *
+ * /categories
+ * /categories/add
+ * /categories/update
+ * /categories/:id/update
+ * /categories/update
+ * /categories/:id/delete
+ *
+ * /employees
+ * /employees/add
+ * /employees/update
+ * /employees/:id/update
+ * /employees/update
+ * /employees/:id/delete
+ *
+ *
+ */
